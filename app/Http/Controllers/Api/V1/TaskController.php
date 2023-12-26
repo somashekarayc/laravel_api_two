a522af6cadeafd09bf4b84361f42c65cdfd7f097
som <somashekarayc@gmail.com>
2023-12-26 22:43:36 +0530

    response implement

--- app/Http/Controllers/Api/V1/TaskController.php
+++ app/Http/Controllers/Api/V1/TaskController.php
@@ -10,52 +10,41 @@ use App\Http\Resources\TaskResource;
 
 class TaskController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+
     public function index()
     {
         return TaskResource::collection(Task::all());
     }
 
-
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(StoreTaskRequest $request)
     {
         $task = Task::create($request->validated());
-        return TaskResource::make($task);
+        return $this->success(['task' => new TaskResource($task)]);
+
+
 
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(Task $task)
     {
-        return TaskResource::make($task);
+        return $this->success(['task' => new TaskResource($task)]);
+
     }
 
-
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(UpdateTaskRequest $request, Task $task)
     {
         $task->update($request->validated());
 
-        return TaskResource::make($task);
+        return $this->success(['task' => new TaskResource($task)]);
+
 
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(Task $task)
     {
         $task->delete();
 
-        return response()->noContent();
+        return $this->success();
+
     }
 }
---
+++ app/Http/Controllers/JaskController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Jask;
+use App\Http\Requests\StoreJaskRequest;
+use App\Http\Requests\UpdateJaskRequest;
+
+class JaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreJaskRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Jask $jask)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Jask $jask)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateJaskRequest $request, Jask $jask)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Jask $jask)
+    {
+        //
+    }
+}
